90441272bdf4301131fd9b32138f7d85a2dd9276
shaju george <shajugeorge45@gmail.com>
2020-06-27 18:51:47 +0530

    entry and Button added

--- calcu.py
+++ calcu.py
@@ -8,7 +8,7 @@ window.title("Calculator")
 def click() :
     pass
 
-
+# buttons
 button0= Button(window,text="0", padx=40,pady=20,command=click)
 button1= Button(window,text="1", padx=40,pady=20,command =click)
 button2= Button(window,text="2" ,padx=40,pady=20,command =click)
@@ -26,25 +26,28 @@ button_div= Button(window,text="/" ,padx=40,pady=20,command =click)
 button_equals= Button(window,text="=" ,padx=40,pady=20,command =click)
 button_clear= Button(window,text="C" ,padx=40,pady=20,command =click)
 
-button0.grid(row=3,column=0)
-button1.grid(row=2,column=0)
-button2.grid(row=2,column=1)
-button3.grid(row=2,column=2)
-button4.grid(row=1,column=0)
-button5.grid(row=1,column=1)
-button6.grid(row=1,column=2)
-button7.grid(row=0,column=0)
-button8.grid(row=0,column=1)
-button9.grid(row=0,column=2)
-button_add.grid(row=3,column=1)
-button_mul.grid(row=4,column=0)
-button_sub.grid(row=3,column=2)
-button_div.grid(row=4,column=1)
-button_equals.grid(row=5,column=0)
-button_clear.grid(row=4,column=2)
+button0.grid(row=4,column=0)
+button1.grid(row=3,column=0)
+button2.grid(row=3,column=1)
+button3.grid(row=3,column=2)
+button4.grid(row=2,column=0)
+button5.grid(row=2,column=1)
+button6.grid(row=2,column=2)
+button7.grid(row=1,column=0)
+button8.grid(row=1,column=1)
+button9.grid(row=1,column=2)
+button_add.grid(row=4,column=1)
+button_mul.grid(row=5,column=0)
+button_sub.grid(row=4,column=2)
+button_div.grid(row=5,column=1)
+button_equals.grid(row=6,column=0)
+button_clear.grid(row=5,column=2)
 
 
+# output
 
+e = Entry (window,width = 36 ,borderwidth =5)
+e.grid(row=0,column =0,columnspan =3,padx=10,pady=20)
 
 
 
