9db76ae5d95b669676ec99aa9f96e3ca980a7600
shaju george <shajugeorge45@gmail.com>
2020-06-27 18:06:50 +0530

    initial commit

---
+++ calcu.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+window=Tk()
+
+window.title("Calculator")
+
+button= Button(window,text="1", padx=40,pady=20)
+button.grid(row=0,column=0)
+
+window.mainloop
