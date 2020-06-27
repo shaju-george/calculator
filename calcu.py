787eadd7f23a8bde7490f4e1a0e944a20b6c9e5c
shaju george <shajugeorge45@gmail.com>
2020-06-28 00:40:16 +0530

    version 2.0

--- calcu.py
+++ calcu.py
@@ -1,9 +1,26 @@
-import tkinter
+
+
+# calculator
+
 from tkinter import *
+import tkinter.ttk as ttk
+
 
 window=Tk()
 
+'''
+style = ttk.Style(window)
+#style.theme_use("clam")
+style.theme_use("classic")
+
+style.map("C.TButton",
+   foreground=[('!active', 'black'),('pressed', 'red'), ('active', 'white')],
+    background=[ ('pressed', 'white'), ('active', '#ffcce6')]
+    )
+'''
+
 window.title("Calculator")
+
 
 # getting input values
 def click(number) :
@@ -67,26 +84,26 @@ def equal () :
         e.insert(0,mem / int(next_value))
 
 # buttons
-button0= Button(window,text="0", padx=40,pady=20,command =lambda:click(0))
-button1= Button(window,text="1", padx=40,pady=20,command =lambda:click(1))
-button2= Button(window,text="2" ,padx=40,pady=20,command =lambda:click(2))
-button3= Button(window,text="3" ,padx=40,pady=20,command =lambda:click(3))
-button4= Button(window,text="4" ,padx=40,pady=20,command =lambda:click(4))
-button5= Button(window,text="5" ,padx=40,pady=20,command =lambda:click(5))
-button6= Button(window,text="6" ,padx=40,pady=20,command =lambda:click(6))
-button7= Button(window,text="7" ,padx=40,pady=20,command =lambda:click(7))
-button8= Button(window,text="8" ,padx=40,pady=20,command =lambda:click(8))
-button9= Button(window,text="9" ,padx=40,pady=20,command =lambda:click(9))
+button0= ttk.Button(window,text="0",command =lambda:click(0))
+button1= ttk.Button(window,text="1",command =lambda:click(1))
+button2= ttk.Button(window,text="2" ,command =lambda:click(2))
+button3= ttk.Button(window,text="3" ,command =lambda:click(3))
+button4= ttk.Button(window,text="4" ,command =lambda:click(4))
+button5= ttk.Button(window,text="5" ,command =lambda:click(5))
+button6= ttk.Button(window,text="6" ,command =lambda:click(6))
+button7= ttk.Button(window,text="7" ,command =lambda:click(7))
+button8= ttk.Button(window,text="8" ,command =lambda:click(8))
+button9= ttk.Button(window,text="9" ,command =lambda:click(9))
 
-button_add= Button(window,text="+" ,padx=40,pady=20,command =add)
-button_mul= Button(window,text="*" ,padx=40,pady=20,command =mul)
-button_sub= Button(window,text="-" ,padx=40,pady=20,command =sub)
-button_div= Button(window,text="/" ,padx=40,pady=20,command =div)
+button_add= ttk.Button(window,text="+" ,command =add)
+button_mul= ttk.Button(window,text="*" ,command =mul)
+button_sub= ttk.Button(window,text="-" ,command =sub)
+button_div= ttk.Button(window,text="/" ,command =div)
 
-button_equals= Button(window,text="=" ,padx=40,pady=20,command =equal)
-button_clear= Button(window,text="C" ,padx=40,pady=20,command =clear)
+button_equals= ttk.Button(window,text="=", command =equal)
+button_clear= ttk.Button(window,text="C" ,command =clear)
 
-button0.grid(row=4,column=0)
+button0.grid(row=4,column=0,padx=0,pady=0)
 button1.grid(row=3,column=0)
 button2.grid(row=3,column=1)
 button3.grid(row=3,column=2)
@@ -99,9 +116,9 @@ button9.grid(row=1,column=2)
 button_add.grid(row=4,column=1)
 button_mul.grid(row=5,column=0)
 button_sub.grid(row=4,column=2)
-button_div.grid(row=5,column=1)
-button_equals.grid(row=6,column=0)
-button_clear.grid(row=5,column=2)
+button_div.grid(row=6,column=0)
+button_equals.grid(row=6,column=1 ,columnspan =2,padx=0)
+button_clear.grid(row=5,column=1,columnspan=2)
 
 
 # output
@@ -113,3 +130,4 @@ e.grid(row=0,column =0,columnspan =3,padx=10,pady=20)
 
 
 window.mainloop
+
