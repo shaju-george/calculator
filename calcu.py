b54ba761c0c61a338edbe16008d1308029bb1487
shaju george <shajugeorge45@gmail.com>
2020-07-12 23:39:33 +0530

    added scientific func

--- calcu.py
+++ calcu.py
@@ -1,10 +1,9 @@
-
-
 # calculator
 
 from tkinter import *
 import tkinter.ttk as ttk
 from tkinter import messagebox
+import math
 
 window=Tk()
 
@@ -16,12 +15,11 @@ style.map("C.TButton",
    foreground=[('!active', 'black'),('pressed', 'red'), ('active', 'white')],
     background=[ ('pressed', 'white'), ('active', '#ffcce6')]
     )
-
 '''
 #s#h#a#j#u
 window.title("Calculator")
 
-window.geometry('425x350')
+window.geometry('425x600')
 window.configure(bg='#f9ecec')
 window.resizable(0,0)
 
@@ -38,11 +36,12 @@ def clear() :
 # add
 def add() :
     global operation
-    operation = 'add'
+#   operation = 'add'
     num =e.get()
-    global mem
-    mem = float(num)
+   # global mem
+   # mem = float(num)
     e.delete(0,END)
+    e.insert(0 ,num + '+')
     
 
 # subtraction
@@ -79,7 +78,7 @@ def equal():
     if operation == "add" :
         e.insert(0,float(value))
         
-        t = float(value)'''
+        t = float(value)
 
 #result
 def equal () :
@@ -102,19 +101,152 @@ def equal () :
         else:
             e.insert(0,mem / float(next_value))
             t = mem / float(next_value)
-
+    if operation == "fact" :
+        e.insert(0,math.factorial(mem))
+        t = math.factorial(mem)
+    if operation == "log" :
+        e.insert(0,math.log10(mem))
+        t = math.log10(mem)
+    if operation == "sin" :
+        e.insert(0,math.sin(mem))
+        t = math.sin(mem)
+    if operation == "tan" :
+        e.insert(0,math.tan(mem))
+        t = math.tan(mem)
+    if operation == "cos" :
+        e.insert(0,math.cos(mem))
+        t = math.cos(mem)
+    if operation == "sqrt" :
+        e.insert(0,math.sqrt(mem))
+        t = math.sqrt(mem)
+    if operation == "pow" :
+        e.insert(0,math.pow(mem,float(next_value)))
+        t = math.pow(mem,float(next_value))
+  '''
+def equal():
+    equation= e.get()
+    e.delete(0,END)
+    e.insert(0,eval(equation))
+    global mem
+    mem = eval(equation)
         
 # Prev answer
 def ans():
     e.delete(0,END)
-    e.insert(0, t)
+    e.insert(0, mem)
 
 #dot button
 def dot():
     temp= e.get()
     e.delete(0,END)
     e.insert (0,str(temp)+".")
+    
+def back():
+    c = e.get()
+    e.delete(0,END)
+    e.insert(0,c[:-1])
 
+def fact():
+    global operation
+    operation = 'fact'
+    num =e.get()
+    global mem
+    mem1 = float(num)
+    mem = int(mem1)
+    e.delete(0,END)
+    e.insert(0,str(mem)+'!')
+
+def log():
+    global operation
+    operation = 'log'
+    num =e.get()
+    global mem
+    mem = float(num)
+    e.delete(0,END)
+    e.insert(0,'log(' +str(num)+')')
+
+
+def sin():
+    global operation
+    operation = 'sin'
+    num =e.get()
+    global mem
+    mem = float(num)
+    e.delete(0,END)
+    e.insert(0,'sin(' +str(num)+')')
+
+def cos():
+    global operation
+    operation = 'cos'
+    num =e.get()
+    global mem
+    mem = float(num)
+    e.delete(0,END)
+    e.insert(0,'cos(' +str(num)+')')
+
+def tan():
+    global operation
+    operation = 'tan'
+    num =e.get()
+    global mem
+    mem = float(num)
+    e.delete(0,END)
+    e.insert(0,'tan(' +str(num)+')')
+
+def sqrt():
+    global operation
+    operation = 'sqrt'
+    num =e.get()
+    global mem
+    mem = float(num)
+    e.delete(0,END)
+    e.insert(0,'sqrt(' +str(num)+')')
+
+
+def pow1():
+    global operation
+    operation = 'pow'
+    num =e.get()
+    global mem
+    mem = float(num)
+    e.delete(0,END)
+    # e.insert(0,(str(num)+'^'))
+    
+def pi():
+    global operation
+    e.delete(0,END)
+    e.insert(0,math.pi)
+
+def math_e():
+    global operation
+    e.delete(0,END)
+    e.insert(0,math.e)
+
+memory = 0
+def m_plus():
+    global memory
+    memory1 = e.get()
+    memory = memory + float(memory1)
+    e.delete(0,END)
+    
+def m_minus():
+    global memory
+    memory1 = e.get()
+    memory = memory - float(memory1)
+    e.delete(0,END)
+
+def mr():
+    global memory
+    e.delete(0,END)
+    if memory == 0:
+        e.insert(0,'')
+    else:
+        e.insert(0,memory)
+    
+def mc():
+    global memory
+    memory = 0
+    e.delete(0,END)
 
 
 # buttons
@@ -138,8 +270,24 @@ button_dot= ttk.Button(window,text="." ,command =dot)
 
 button_equals= ttk.Button(window,text="=", command =equal)
 button_clear= ttk.Button(window,text="C" ,command =clear)
+
+button_back= ttk.Button(window,text="<-" ,command =back)
 button_ans= ttk.Button(window,text="ans",command =ans)
 
+button_fact= ttk.Button(window,text="x!",command =fact)
+button_log= ttk.Button(window,text="log10",command =log)
+button_sin= ttk.Button(window,text="sin",command =sin)
+button_cos= ttk.Button(window,text="cos",command =cos)
+button_tan= ttk.Button(window,text="tan",command =tan)
+button_sqrt= ttk.Button(window,text="√",command =sqrt)
+button_pow= ttk.Button(window,text="x^",command = pow1)
+button_pi= ttk.Button(window,text="π",command = pi)
+button_e= ttk.Button(window,text="e",command = math_e)
+
+button_mr= ttk.Button(window,text="mr",command = mr)
+button_mc= ttk.Button(window,text="mc",command = mc)
+button_mp = ttk.Button(window,text="m+",command = m_plus)
+button_mm = ttk.Button(window,text="m-",command = m_minus)
 #button0.shaju.place(height=80, width=100, x=300, y=300)
 
 button0.grid(row=4,column=1,padx=5,pady=5,ipadx=10,ipady=10)
@@ -160,8 +308,20 @@ button_equals.grid(row=5,column=2 ,columnspan =2,padx=5,pady=5,ipadx=50,ipady=10
 button_clear.grid(row=4,column=2,padx=5,pady=5,ipadx=10,ipady=10)
 button_ans.grid(row=4,column=0,padx=5,pady=5,ipadx=10,ipady=10)
 button_dot.grid(row=5,column=0,padx=5,pady=5,ipadx=10,ipady=10)
-
-
+button_back.grid(row=5,column=1,padx=5,pady=5,ipadx=10,ipady=10)
+button_fact.grid(row=6,column=1,padx=5,pady=5,ipadx=10,ipady=10)
+button_sqrt.grid(row=6,column=2,padx=5,pady=5,ipadx=10,ipady=10)
+button_log.grid(row=6,column=3,padx=5,pady=5,ipadx=10,ipady=10)
+button_sin.grid(row=6,column=0,padx=5,pady=5,ipadx=10,ipady=10)
+button_cos.grid(row=7,column=0,padx=5,pady=5,ipadx=10,ipady=10)
+button_tan.grid(row=7,column=1,padx=5,pady=5,ipadx=10,ipady=10)
+button_pow.grid(row=7,column=2,padx=5,pady=5,ipadx=10,ipady=10)
+button_pi.grid(row=7,column=3,padx=5,pady=5,ipadx=10,ipady=10)
+button_e.grid(row=8,column=0,padx=5,pady=5,ipadx=10,ipady=10)
+button_mr.grid(row=9,column=0,padx=5,pady=5,ipadx=10,ipady=10)
+button_mc.grid(row=9,column=1,padx=5,pady=5,ipadx=10,ipady=10)
+button_mp.grid(row=9,column=2,padx=5,pady=5,ipadx=10,ipady=10)
+button_mm.grid(row=9,column=3,padx=5,pady=5,ipadx=10,ipady=10)
 # output
 
 e = Entry (window,width = 36 ,borderwidth =5)
@@ -170,6 +330,3 @@ e.grid(row=0,column =0,columnspan =4,ipadx=40,pady=10,ipady=10)
 
 
 window.mainloop
-
-
-
